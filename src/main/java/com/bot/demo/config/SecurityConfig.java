0318bc7ce1e946776be335efe750f266137c9b63
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2022-04-13 23:33:12 +0900

    security disable

--- src/main/java/com/bot/demo/config/SecurityConfig.java
+++ src/main/java/com/bot/demo/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable();
+        security
+            .authorizeRequests().antMatchers("/**").permitAll().and()
+            .csrf().disable()
+            .httpBasic().disable();
+
     }
+
 }
