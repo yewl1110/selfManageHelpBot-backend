478bd82a5286fc42b8262207d354a37eeef01cb7
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2022-05-15 00:28:14 +0900

    tmp

--- src/main/java/com/bot/demo/respository/UsersRepo.java
+++ src/main/java/com/bot/demo/respository/UsersRepo.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface UsersRepo extends MongoRepository<User, String> {
     User findByUserId(String Id);
+    void deleteUserByUserId(String userId);
 //    void deleteTodosUserIdAndAccessKey
     // update
     User insert(User user);
